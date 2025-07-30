a1295c6c305b47c55215c66ee422e215b09bf8e3
Soham <sohambhogaonkar20@gmail.com>
2025-07-30 23:37:26 +0530

    Exp4_Bitwisesetting.cpp

---
+++ Exp4_Bitwisesetting.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int number, bitPosition;
+    cout << "Enter an integer: ";
+    cin >> number;
+    cout << "Enter bit position to modify: ";
+    cin >> bitPosition;
+    cout << "After setting bit " << bitPosition << ": " << (number | (1 << bitPosition)) << endl;
+    cout << "After resetting bit " << bitPosition << ": " << (number & (~(1 << bitPosition))) << endl;
+    return 0;
+}
+/*OUTPUT
+Enter an integer: 11
+Enter bit position to modify: 0
+After setting bit 0: 11
+After resetting bit 0: 10 */
